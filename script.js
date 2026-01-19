90b71ff89fc4c5cca504b99c9c03517a64214388
guantonamous <rolandocahill@gmail.com>
2026-01-19 17:56:17 -0500

    001 Update script.js
    -Fixed form Redirection

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const navLinks = document.getElementById("nav-links");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+const form = document.getElementById("contact-form");
+const status = document.getElementById("status");
+
+form.addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const data = new FormData(form);
+
+  const response = await fetch("https://formspree.io/f/mbddybnb", {
+    method: "POST",
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    status.textContent = "Message sent successfully!";
+    form.reset();
+  } else {
+    status.textContent = "Oops! There was a problem.";
+  }
+});
+
